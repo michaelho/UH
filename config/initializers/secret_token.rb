cb8c6fed1b81295cef964ba5f983e078f86777ed
Michael Ho <mho@honolulu.gov>
2011-10-14 13:37:04 -1000

    new UH website created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UH::Application.config.secret_token = '6be688e800c596b716d122650c6efd16067d08c3ddd31d02f9c8eac1b430033944ce9bec89c54d570ad474ac40181737659bd2b799d1f64d93ad03e2fb660163'
